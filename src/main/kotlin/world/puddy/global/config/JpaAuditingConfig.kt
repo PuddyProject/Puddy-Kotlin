c5b7a3382935bc7a1fcbfad78a901d94306485b1
waveofmymind <sjun0913@gmail.com>
2023-06-05 01:01:14 +0900

    Feat: Auditing 설정 추가

---
+++ src/main/kotlin/world/puddy/global/config/JpaAuditingConfig.kt
@@ -0,0 +1,9 @@
+package world.puddy.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaAuditingConfig {
+}
