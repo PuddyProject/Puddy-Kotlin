4dd67a7abb90bf118cedd530b3aa64c186f5f721
waveofmymind <sjun0913@gmail.com>
2023-07-14 09:24:14 +0900

    feat: 컬럼명 추가

--- question/src/main/kotlin/world/puddy/domain/Question.kt
+++ question/src/main/kotlin/world/puddy/domain/Question.kt
@@ -6,18 +6,22 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Lob
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "question")
 class Question(
 
     @Column(name = "title", length = 50)
     private val title: String,
 
     @Lob
+    @Column(name = "content")
     private val content: String,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
     private val id: Long = 0L
 ) {
     init {
