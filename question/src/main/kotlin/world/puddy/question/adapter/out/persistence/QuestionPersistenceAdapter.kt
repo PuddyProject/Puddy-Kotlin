23ea68c9fc23c56c6cafba1ebd39c311171e054e
waveofmymind <sjun0913@gmail.com>
2023-07-14 23:07:27 +0900

    feat: 트랜잭션 추가

--- question/src/main/kotlin/world/puddy/question/adapter/out/persistence/QuestionPersistenceAdapter.kt
+++ question/src/main/kotlin/world/puddy/question/adapter/out/persistence/QuestionPersistenceAdapter.kt
@@ -1,13 +1,18 @@
 package world.puddy.question.adapter.out.persistence
 
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import world.puddy.question.application.port.`in`.RegisterQuestionCommand
 import world.puddy.question.application.port.out.RegisterQuestionPort
 import world.puddy.question.domain.Question
 
+@Service
+@Transactional(readOnly = true)
 class QuestionPersistenceAdapter(
     private val questionRepository: QuestionRepository,
 ) : RegisterQuestionPort {
 
+    @Transactional
     override fun registerQuestion(command: RegisterQuestionCommand): Question {
         return questionRepository.save(QuestionMapper.toEntity(command))
     }
