a3622444cbb327c3dbd906ce9f69d030120d1f19
waveofmymind <sjun0913@gmail.com>
2023-07-15 15:33:33 +0900

    fix: 사용하지 않은 import 삭제

--- question/src/main/kotlin/world/puddy/question/adapter/in/web/RegisterQuestionController.kt
+++ question/src/main/kotlin/world/puddy/question/adapter/in/web/RegisterQuestionController.kt
@@ -1,7 +1,6 @@
 package world.puddy.question.adapter.`in`.web
 
 import org.springframework.http.MediaType
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
