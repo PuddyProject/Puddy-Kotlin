05ddf0a01bd2a82106069997a033df7bf6197e67
waveofmymind <sjun0913@gmail.com>
2023-07-17 13:46:29 +0900

    refactor: 변환 로직을 DTO의 스태틱 메서드를 사용

--- question/src/main/kotlin/world/puddy/question/adapter/in/web/FindQuestionResponse.kt
+++ question/src/main/kotlin/world/puddy/question/adapter/in/web/FindQuestionResponse.kt
@@ -1,9 +1,26 @@
 package world.puddy.question.adapter.`in`.web
 
+import world.puddy.question.domain.Question
+import world.puddy.question.domain.validator.Category
+
 data class FindQuestionResponse(
     val id: Long,
     val title: String,
     val content: String,
+    @Category
     val category: String,
     val postCategory: Int,
-)
+) {
+    companion object {
+        @JvmStatic
+        fun of(question: Question): FindQuestionResponse {
+            return FindQuestionResponse(
+                id = question.id,
+                title = question.title,
+                content = question.content,
+                category = question.category.name,
+                postCategory = question.postCategory
+            )
+        }
+    }
+}
--- question/src/main/kotlin/world/puddy/question/application/service/FindQuestionService.kt
+++ question/src/main/kotlin/world/puddy/question/application/service/FindQuestionService.kt
@@ -3,7 +3,6 @@ package world.puddy.question.application.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import world.puddy.question.adapter.`in`.web.FindQuestionResponse
-import world.puddy.question.adapter.out.persistence.QuestionMapper
 import world.puddy.question.application.port.`in`.FindQuestionUseCase
 import world.puddy.question.application.port.out.FindQuestionPort
 
@@ -11,8 +10,7 @@ import world.puddy.question.application.port.out.FindQuestionPort
 @Transactional(readOnly = true)
 class FindQuestionService(
     private val findQuestionPort: FindQuestionPort,
-    private val questionMapper: QuestionMapper
 ) : FindQuestionUseCase {
     override fun findQuestion(id: Long): FindQuestionResponse =
-        questionMapper.toResponse(findQuestionPort.findQuestion(id))
+        FindQuestionResponse.of(findQuestionPort.findQuestion(id))
 }
