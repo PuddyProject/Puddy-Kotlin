7fa87e79c269d5a83e0196d526d048ffef3ed19e
waveofmymind <sjun0913@gmail.com>
2023-07-18 16:59:21 +0900

    feat: 조회 비즈니스 로직 테스트 추가

---
+++ question/src/test/kotlin/world/puddy/question/application/service/FindQuestionServiceTest.kt
@@ -0,0 +1,43 @@
+package world.puddy.question.application.service
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import world.puddy.common.error.exception.QuestionNotFoundException
+import world.puddy.question.QuestionSnippets
+import world.puddy.question.application.port.out.FindQuestionPort
+
+class FindQuestionServiceTest : BehaviorSpec({
+    val findQuestionPort = mockk<FindQuestionPort>()
+    val findQuestionService = FindQuestionService(findQuestionPort)
+    val mockMvc = MockMvcBuilders.standaloneSetup(findQuestionService).build()
+
+    given("질문 조회 서비스에서") {
+        val questionId = 1L
+        val response = QuestionSnippets.findQuestionResponse()
+        every { findQuestionPort.findQuestion(questionId) } answers { QuestionSnippets.question() }
+        `when`("올바른 id로 조회했을 때") {
+            val actual = findQuestionService.findQuestion(questionId)
+            then("해당 id에 대한 질문글을 반환한다.") {
+                actual.id shouldBe response.id
+                actual.title shouldBe response.title
+                actual.content shouldBe response.content
+                actual.category shouldBe response.category
+                actual.postCategory shouldBe response.postCategory
+            }
+        }
+        `when`("없는 id일 경우") {
+            val notRegisterId = 2L
+            val exception = QuestionNotFoundException()
+            every { findQuestionPort.findQuestion(notRegisterId) } answers { throw exception }
+            then("QuestionNotFoundException이 발생한다.") {
+                shouldThrow<QuestionNotFoundException> {
+                    findQuestionService.findQuestion(notRegisterId)
+                }.message shouldBe exception.message
+            }
+        }
+    }
+})
