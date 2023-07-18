588ae5aafd6c5fda5d9bc441ba2243b9c9bb3f75
waveofmymind <sjun0913@gmail.com>
2023-07-18 21:43:46 +0900

    feat: @RequestMapping으로 일관성 유지

--- question/src/main/kotlin/world/puddy/question/adapter/in/web/EditQuestionController.kt
+++ question/src/main/kotlin/world/puddy/question/adapter/in/web/EditQuestionController.kt
@@ -3,6 +3,7 @@ package world.puddy.question.adapter.`in`.web
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
@@ -10,11 +11,12 @@ import world.puddy.common.response.Response
 import world.puddy.question.application.port.`in`.EditQuestionUseCase
 
 @RestController
+@RequestMapping("/questions")
 class EditQuestionController(
     private val editQuestionUseCase: EditQuestionUseCase
 ) {
     @PutMapping(
-        "/questions/{questionId}",
+        "/{questionId}",
         consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE]
     )
     fun editQuestion(
