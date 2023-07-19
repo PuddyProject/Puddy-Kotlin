82ff0cce7f4e84386b4f3559c6d43a4f19d3a6c0
waveofmymind <sjun0913@gmail.com>
2023-07-19 13:00:37 +0900

    fix: 모듈을 탐색 못하는 이슈 해결

--- api/src/main/kotlin/world/puddy/PuddyApplication.kt
+++ api/src/main/kotlin/world/puddy/PuddyApplication.kt
@@ -4,9 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 import world.puddy.question.QuestionConfigurationLoader
+import world.puddy.user.UserConfigurationLoader
 
 @SpringBootApplication
-@Import(QuestionConfigurationLoader::class)
+@Import(QuestionConfigurationLoader::class, UserConfigurationLoader::class)
 class PuddyApplication
 
 fun main(args: Array<String>) {
