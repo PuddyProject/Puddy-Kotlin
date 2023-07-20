60bfd44ace2c47027082de04a5100ae42493b2e3
waveofmymind <sjun0913@gmail.com>
2023-07-21 01:00:31 +0900

    feat: Spring Security 의존성 및 SecurityConfig 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,4 +1,3 @@
-
 val jar: Jar by tasks
 val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
 
@@ -8,4 +7,8 @@ jar.enabled = false
 dependencies {
     api(project(":question"))
     api(project(":user"))
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.security:spring-security-oauth2-client")
 }
--- api/src/main/kotlin/world/puddy/PuddyApplication.kt
+++ api/src/main/kotlin/world/puddy/PuddyApplication.kt
@@ -3,10 +3,12 @@ package world.puddy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import world.puddy.question.QuestionConfigurationLoader
 import world.puddy.user.UserConfigurationLoader
 
 @SpringBootApplication
+@EnableWebSecurity
 @Import(QuestionConfigurationLoader::class, UserConfigurationLoader::class)
 class PuddyApplication
 
