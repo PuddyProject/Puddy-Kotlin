993a34bfcdcf209eb209a167ec5cbffbb1f2b082
waveofmymind <sjun0913@gmail.com>
2023-08-07 14:42:35 +0900

    refactor: 팩토리 메서드를 사용해서 커맨드 생성

--- puddy-api/src/main/kotlin/world/puddy/api/question/RegisterQuestionController.kt
+++ puddy-api/src/main/kotlin/world/puddy/api/question/RegisterQuestionController.kt
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import world.puddy.common.response.Response
-import world.puddy.core.domain.question.application.port.`in`.RegisterQuestionCommand
 import world.puddy.core.domain.question.application.port.`in`.RegisterQuestionUseCase
+import world.puddy.core.global.response.Response
 
 @RestController
 @RequestMapping("/questions")
@@ -21,12 +20,9 @@ class RegisterQuestionController(
         @RequestPart("request") request: RegisterQuestionRequest,
         @RequestPart(value = "images", required = false) images: List<MultipartFile>?
     ): Response<Long> {
-        RegisterQuestionCommand(
-            title = request.title,
-            content = request.content,
-            category = request.category,
-            postCategory = request.postCategory,
-            images = images
+        request.toCommand(
+            memberId = 1L,
+            images = emptyList()
         ).also { command ->
             return Response.ok(registerQuestionUseCase.registerQuestion(command))
         }
