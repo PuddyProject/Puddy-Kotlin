3c1aed6fd43bf5341679316bafc5e615abda9533
waveofmymind <sjun0913@gmail.com>
2023-08-07 21:28:11 +0900

    refactor: 패키지 스캔은 현재 속한 모듈도 추가되어야 한다.

--- puddy-api/src/main/kotlin/world/puddy/PuddyApplication.kt
+++ puddy-api/src/main/kotlin/world/puddy/PuddyApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(
-    scanBasePackages = ["world.puddy.core", "world.puddy.common"]
+    scanBasePackages = ["world.puddy.core", "world.puddy.api"]
 )
 class PuddyApplication
 
