04f9a06d89cd27142510360d56431cdcb7514d80
waveofmymind <sjun0913@gmail.com>
2023-08-08 09:06:06 +0900

    feat: 유저 로그인 로직을 Kotest와 Mockk를 통해 검증할 수 있다.

--- puddy-core/src/test/kotlin/world/puddy/domain/user/UserFixture.kt
+++ puddy-core/src/test/kotlin/world/puddy/domain/user/UserFixture.kt
@@ -1,6 +1,12 @@
 package world.puddy.domain.user
 
 import world.puddy.core.domain.user.application.port.`in`.JoinUserCommand
+import world.puddy.core.domain.user.application.port.`in`.LoginUserCommand
+import world.puddy.core.domain.user.domain.Password
+import world.puddy.core.domain.user.domain.User
+import world.puddy.core.domain.user.domain.UserRole
+import world.puddy.core.global.jwt.CreateTokenCommand
+import world.puddy.core.global.jwt.LoginToken
 
 object UserFixture {
     fun joinUserCommand(email: String) = JoinUserCommand(
@@ -9,5 +15,35 @@ object UserFixture {
         email = email,
         password = "1234",
         notificated = true,
+    )
+
+    fun loginUserCommand(account: String, password: String): LoginUserCommand {
+        return LoginUserCommand(
+            account = account,
+            password = password,
+        )
+    }
+
+    fun user(password: String) = User(
+        id = 1L,
+        account = "test",
+        username = "test",
+        email = "test@test.com",
+        password = Password(password),
+        notificated = true
+    )
+
+    fun createTokenRequest() = CreateTokenCommand(
+        account = "test",
+        username = "test",
+        nickname = "test",
+        role = UserRole.ROLE_USER.name,
+        id = 1L,
+        email = "test@test.com",
+    )
+
+    fun loginToken() = LoginToken(
+        accessToken = "Bearer this-is-access-token",
+        refreshToken = "Bearer this-is-refresh-token",
     )
 }
--- puddy-core/src/test/kotlin/world/puddy/domain/user/application/UserServiceTest.kt
+++ puddy-core/src/test/kotlin/world/puddy/domain/user/application/UserServiceTest.kt
@@ -2,14 +2,13 @@ package world.puddy.domain.user.application
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import io.mockk.clearMocks
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import world.puddy.core.domain.user.application.port.out.FindUserPort
 import world.puddy.core.domain.user.application.port.out.JoinUserPort
 import world.puddy.core.domain.user.application.service.UserCommandService
+import world.puddy.core.global.error.ErrorCode
 import world.puddy.core.global.error.exception.DuplicateRegisterException
+import world.puddy.core.global.error.exception.UnidentifiedUserException
 import world.puddy.core.global.jwt.JwtTokenProvider
 import world.puddy.domain.user.UserFixture
 
@@ -44,5 +43,37 @@ class UserServiceTest : BehaviorSpec({
                 verify(exactly = 0) { joinUserPort.saveUser(any()) }
             }
         }
+    }
+
+    given("로그인을 요청할 때") {
+        `when`("비밀번호가 틀린 경우") {
+            val command = UserFixture.loginUserCommand("test", "wrongPassword")
+            val user = spyk(UserFixture.user("1234"))
+            every { findUserPort.getUserByAccount("test") } returns user
+            every { user.authenticate(any()) } throws UnidentifiedUserException(ErrorCode.INVALID_PASSWORD)
+            then("로그인이 실패한다") {
+                shouldThrow<UnidentifiedUserException> {
+                    userService.login(command)
+                }
+                verify(exactly = 1) { findUserPort.getUserByAccount("test") }
+                verify(exactly = 1) { user.authenticate(any()) }
+                verify(exactly = 0) { jwtTokenProvider.createToken(any()) }
+                clearMocks(findUserPort, user, jwtTokenProvider)
+            }
+        }
+        `when`("올바른 정보인 경우") {
+            val command = UserFixture.loginUserCommand("test", "1234")
+            val user = UserFixture.user("1234")
+            val loginToken = UserFixture.loginToken()
+            every { findUserPort.getUserByAccount(any()) } returns user
+            every { jwtTokenProvider.createToken(any()) } returns loginToken
+            then("로그인이 성공한다.") {
+                userService.login(command)
+                verify(exactly = 1) { findUserPort.getUserByAccount("test") }
+                verify(exactly = 1) { jwtTokenProvider.createToken(any()) }
+            }
+        }
+
+
     }
 })
