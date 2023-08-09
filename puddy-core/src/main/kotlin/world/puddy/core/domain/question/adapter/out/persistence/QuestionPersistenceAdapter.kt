0f8fa33415c212e2112961b4c46edee3cd333b0d
waveofmymind <sjun0913@gmail.com>
2023-08-10 00:53:47 +0900

    refactor: 게시글이 edit을 실행하는 것은 비즈니스 레이어인 Service에서 이루어져야한다.

--- puddy-core/src/main/kotlin/world/puddy/core/domain/question/adapter/out/persistence/QuestionPersistenceAdapter.kt
+++ puddy-core/src/main/kotlin/world/puddy/core/domain/question/adapter/out/persistence/QuestionPersistenceAdapter.kt
@@ -3,18 +3,18 @@ package world.puddy.core.domain.question.adapter.out.persistence
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
-import world.puddy.core.global.error.exception.QuestionNotFoundException
-import world.puddy.core.domain.question.application.port.`in`.EditQuestionCommand
 import world.puddy.core.domain.question.application.port.`in`.RegisterQuestionCommand
-import world.puddy.core.domain.question.application.port.out.*
+import world.puddy.core.domain.question.application.port.out.DeleteQuestionPort
+import world.puddy.core.domain.question.application.port.out.FindQuestionPort
+import world.puddy.core.domain.question.application.port.out.RegisterQuestionPort
 import world.puddy.core.domain.question.domain.Question
+import world.puddy.core.global.error.exception.QuestionNotFoundException
 
 @Repository
 class QuestionPersistenceAdapter(
     private val questionRepository: QuestionRepository,
 ) : RegisterQuestionPort,
     FindQuestionPort,
-    EditQuestionPort,
     DeleteQuestionPort {
 
     override fun registerQuestion(command: RegisterQuestionCommand): Long =
@@ -26,14 +26,6 @@ class QuestionPersistenceAdapter(
 
     override fun findQuestionList(page: Pageable, keyword: String, sort: String): List<Question> =
         questionRepository.findAll()
-
-    override fun editQuestion(
-        command: EditQuestionCommand
-    ): Long {
-        val question = questionRepository.findByIdOrNull(command.questionId) ?: throw QuestionNotFoundException()
-        question.verify(command.memberId)
-        return question.edit(command.title, command.content, command.category)
-    }
 
     override fun deleteQuestion(id: Long) {
         questionRepository.deleteById(id)
--- puddy-core/src/main/kotlin/world/puddy/core/domain/question/application/service/EditQuestionService.kt
+++ puddy-core/src/main/kotlin/world/puddy/core/domain/question/application/service/EditQuestionService.kt
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import world.puddy.core.domain.question.application.port.`in`.EditQuestionCommand
 import world.puddy.core.domain.question.application.port.`in`.EditQuestionUseCase
-import world.puddy.core.domain.question.application.port.out.EditQuestionPort
+import world.puddy.core.domain.question.application.port.out.FindQuestionPort
 
 @Service
 @Transactional
 class EditQuestionService(
-    private val editQuestionPort: EditQuestionPort
+    private val findQuestionPort: FindQuestionPort
 ) : EditQuestionUseCase {
-    override fun editQuestion(command: EditQuestionCommand) = editQuestionPort.editQuestion(command)
+    override fun editQuestion(command: EditQuestionCommand): Long {
+        val question = findQuestionPort.findQuestion(command.questionId)
+        question.assertOwnedBy(command.memberId)
+        return question.edit(command.title, command.content, command.category)
+    }
 }
