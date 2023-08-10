462d79203a50ecf3aa7c6a3568722d22d15ba663
waveofmymind <sjun0913@gmail.com>
2023-08-10 19:24:16 +0900

    refactor: 사용하지 않는 Import 삭제

--- puddy-core/src/main/kotlin/world/puddy/core/global/config/SecurityConfig.kt
+++ puddy-core/src/main/kotlin/world/puddy/core/global/config/SecurityConfig.kt
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import world.puddy.core.domain.user.domain.UserRole
 import world.puddy.core.global.jwt.JwtAuthenticationEntryPoint
 import world.puddy.core.global.jwt.JwtAuthorizationFilter
 
@@ -19,15 +16,12 @@ class SecurityConfig(
 ) {
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
-
-    @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain = http.csrf { it.disable() }
         .sessionManagement { it.disable() }
         .authorizeHttpRequests {
-            it.requestMatchers(HttpMethod.POST, "/questions/**").hasRole("USER")
+            it.requestMatchers(HttpMethod.POST, "/questions/**").hasAnyRole("USER", "EXPERT")
+                .requestMatchers(HttpMethod.PUT, "/questions/**").hasAnyRole("USER", "EXPERT")
+                .requestMatchers(HttpMethod.DELETE, "/questions/**").hasAnyRole("USER", "EXPERT")
                 .requestMatchers("/users/**").permitAll()
                 .anyRequest().permitAll()
         }
