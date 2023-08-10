94604a1d14fe18523c6497980b8a496463d6400b
waveofmymind <sjun0913@gmail.com>
2023-08-10 19:34:33 +0900

    feat: 패스워드 필드를 private로 하여 외부에서 접근시키지 않는다.

--- puddy-api/src/test/kotlin/world/puddy/api/domain/user/domain/UserTest.kt
+++ puddy-api/src/test/kotlin/world/puddy/api/domain/user/domain/UserTest.kt
@@ -3,7 +3,6 @@ package world.puddy.api.domain.user.domain
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import world.puddy.api.domain.user.UserFixture
 import world.puddy.core.domain.user.domain.Password
 import world.puddy.core.domain.user.domain.User
 
@@ -23,14 +22,5 @@ class UserTest : StringSpec({
         user.username shouldBe "test"
         user.nickname shouldBe "퍼디1234"
         user.account shouldBe "test"
-    }
-
-    "패스워드가 암호화된다" {
-        // given
-        val user = UserFixture.user("test")
-        val requestPassword = Password("test")
-        // when & then
-        user.password.value shouldNotBe "test"
-        requestPassword.value shouldBe user.password.value
     }
 })
