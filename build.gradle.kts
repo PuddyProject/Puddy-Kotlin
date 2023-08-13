50056266bc471cf6ab852f5009ae4d42c07a4af1
waveofmymind <sjun0913@gmail.com>
2023-08-13 19:24:48 +0900

    feat: kotlin jdsl 의존성을 추가한다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,6 +81,13 @@ subprojects {
         testImplementation("io.kotest:kotest-assertions-core-jvm:5.6.2")
         testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.2")
         testImplementation("io.mockk:mockk:1.13.5")
+        // kotlin jdsl
+        implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-core-jakarta:2.2.1.RELEASE")
+        implementation("com.linecorp.kotlin-jdsl:kotlin-jdsl-query-jakarta:2.2.1.RELEASE")
+        implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-autoconfigure-jakarta:2.2.1.RELEASE")
+
+
+
 
         // redis
         implementation("org.springframework.boot:spring-boot-starter-data-redis")
