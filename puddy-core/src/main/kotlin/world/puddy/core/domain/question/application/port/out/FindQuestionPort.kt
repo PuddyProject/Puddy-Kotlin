0afdbe21fb13fdb572f214e3bf39d6c71f2229b2
waveofmymind <sjun0913@gmail.com>
2023-08-18 09:02:24 +0900

    refactor(question): 다음 페이지 유무를 판단하기 위해서 Slice를 활용한다.

--- puddy-core/src/main/kotlin/world/puddy/core/domain/question/application/port/out/FindQuestionPort.kt
+++ puddy-core/src/main/kotlin/world/puddy/core/domain/question/application/port/out/FindQuestionPort.kt
@@ -1,10 +1,11 @@
 package world.puddy.core.domain.question.application.port.out
 
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import world.puddy.core.domain.question.domain.Question
 
 interface FindQuestionPort {
     fun findQuestion(id: Long): Question
 
-    fun findQuestionList(page: Pageable, keyword: String, sort: String): List<Question>
+    fun findQuestionList(page: Pageable, keyword: String, sort: String): Slice<Question>
 }
