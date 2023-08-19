9815e7613626fa0c6d95bcd2dbe858a02bdeae6a
waveofmymind <sjun0913@gmail.com>
2023-08-19 14:54:04 +0900

    feat(question): 이미지 픽스처를 추가하고, 질문글 등록 단위테스트를 추가한다.

--- puddy-api/src/test/kotlin/world/puddy/api/domain/question/QuestionFixture.kt
+++ puddy-api/src/test/kotlin/world/puddy/api/domain/question/QuestionFixture.kt
@@ -1,17 +1,62 @@
 package world.puddy.api.domain.question
 
+import org.springframework.mock.web.MockMultipartFile
+import world.puddy.core.domain.image.domain.Image
 import world.puddy.core.domain.question.application.port.`in`.RegisterQuestionCommand
 import world.puddy.core.domain.question.domain.Category
+import world.puddy.core.domain.question.domain.Question
 
 object QuestionFixture {
 
     fun registerQuestionCommand(): RegisterQuestionCommand {
+        val image1 = MockMultipartFile(
+            "file1", // 파일 이름
+            "file1.png", // 원본 파일 이름
+            "image/png", // 컨텐츠 타입
+            byteArrayOf(0) // 파일의 내용, 여기서는 단순 예제로 비어있는 바이트 배열 사용
+        )
+
+        val image2 = MockMultipartFile(
+            "file2",
+            "file2.png",
+            "image/png",
+            byteArrayOf(0)
+        )
         return RegisterQuestionCommand(
             title = "질문글 제목",
             content = "질문글 내용",
             category = Category.건강,
             postCategory = 1,
-            images = null
+            images = listOf(image1, image2)
+        )
+    }
+
+    fun question(): Question {
+        return Question(
+            1L,
+            "질문글 제목",
+            "질문글 내용",
+            Category.건강,
+            1,
+            images(),
+            1L
+        )
+    }
+
+    private fun image(id: Long): Image {
+        return Image(
+            "image.jpg",
+            "original.jpg",
+            "stored.jpg",
+            id
+        )
+    }
+
+    fun images(): MutableList<Image> {
+        return mutableListOf(
+            image(1L),
+            image(2L),
+            image(3L)
         )
     }
 }
--- puddy-api/src/test/kotlin/world/puddy/api/domain/question/application/QuestionServiceTest.kt
+++ puddy-api/src/test/kotlin/world/puddy/api/domain/question/application/QuestionServiceTest.kt
@@ -3,7 +3,7 @@ package world.puddy.api.domain.question.application
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.verifySequence
 import world.puddy.api.domain.question.QuestionFixture
 import world.puddy.core.domain.image.application.port.`in`.SaveImageUseCase
 import world.puddy.core.domain.question.application.port.out.RegisterQuestionPort
@@ -12,21 +12,25 @@ import world.puddy.core.domain.question.application.service.RegisterQuestionServ
 class QuestionServiceTest : BehaviorSpec({
     val registerQuestionPort = mockk<RegisterQuestionPort>()
     val saveImageUseCase = mockk<SaveImageUseCase>()
-    val questionService = RegisterQuestionService(registerQuestionPort,saveImageUseCase)
+    val questionService = RegisterQuestionService(registerQuestionPort, saveImageUseCase)
 
-    given("질문글을 등록할 때") {
-        val command = QuestionFixture.registerQuestionCommand()
-        `when`("모든 정보가 포함되면") {
-            every { registerQuestionPort.registerQuestion(any()) } returns 1L
-            then("질문글이 등록된다") {
+    Given("질문글을 등록하는 경우") {
+        When("올바른 요청 폼일 때") {
+            val question = QuestionFixture.question()
+            val images = QuestionFixture.images()
+            val command = QuestionFixture.registerQuestionCommand()
+
+            every { registerQuestionPort.registerQuestion(any()) } returns question
+            every { saveImageUseCase.saveImage(any()) } returns images
+            Then("질문글이 생성된다.") {
                 questionService.registerQuestion(command)
-                verify(exactly = 1) { registerQuestionPort.registerQuestion(command) }
-            }
-            and("이미지가 포함되지 않으면 이미지 저장 함수가 실행되지 않는다.") {
-                verify(exactly = 0) { saveImageUseCase.saveImage(any()) }
+
+                verifySequence {
+                    registerQuestionPort.registerQuestion(any())
+                    saveImageUseCase.saveImage(any())
+                    registerQuestionPort.registerQuestion(any())
+                }
             }
         }
     }
-
-
 })
